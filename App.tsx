7fdd73b07ec5cce022262c1173cd76ea9c568d80
MarcoAntonioRudasNapoli <marco.rudas@polijunior.com.br>
2021-03-24 23:57:13 -0300

    add: landing page

--- App.tsx
+++ App.tsx
@@ -1,21 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Landing from './src/pages/Landing/index'
+
+import AppLoading from 'expo-app-loading';
+import {Poppins_400Regular, Poppins_600SemiBold, useFonts} from '@expo-google-fonts/poppins'
+import {Archivo_400Regular, Archivo_700Bold} from '@expo-google-fonts/archivo'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  let [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold
+  })
+  
+  if (!fontsLoaded) { //enquanto as fontes não estiverem carregadas
+    return <AppLoading/>;
+  } else {
+    return (
+      <>
+        <Landing/>
+        <StatusBar style="light" />
+      </>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
